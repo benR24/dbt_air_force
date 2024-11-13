5fdb8d8936089cf67ea3eeab2abcc7b682614708
benR24 <benedict.reymann@gmail.com>
2024-11-13 15:37:04 +0100

    s

--- models/staging/staging_flights_filtered_by_airports_raw.sql
+++ models/staging/staging_flights_filtered_by_airports_raw.sql
@@ -1,3 +1,3 @@
 with staging_flights_filtered_by_airports_raw as (
 SELECT * 
-FROM {{ source('air_force', 'flights_filtered_by_airports_raw') }})
+FROM {{ source('staging_flights_filtered_by_airports_raw', 'flights_filtered_by_airports_raw') }})
