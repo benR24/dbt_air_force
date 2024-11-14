a19b50c8e25be843949006a9f58990bb1a1ea930
benR24 <benedict.reymann@gmail.com>
2024-11-14 10:54:37 +0100

    s

---
+++ models/prep/flights_weather_joined.sql
@@ -0,0 +1,15 @@
+with flights_data as(
+    select*
+    from {{ref('flights_filtered_by_airports_cleaned')}}),
+weather_data as(
+    select*
+    from {{ref('staging_weather_daily')}}),
+flights_weather_joined as(
+    (select*
+    from flights_data fc
+    join weather_data wd
+    on (fc.origin = wd.airport_code or fc.dest = wd.airport_code)
+    and fc.flight_date = wd.date
+    order by flight_date asc))
+select*
+from flights_weather_joined;
